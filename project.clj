2e0340c09f25d9096a9824d2fb98dbfe31762894
Ludger Solbach <ludger.solbach@msg.group>
2023-03-15 20:07:17 +0100

    added io namespace with EDN, CSV and JSON support

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
   ; use leiningen dependencies
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/data.json "2.4.0"]
+                 [org.clojure/data.csv "1.0.1"]
                  [org.clojure/tools.cli "1.0.206"]
                  [org.soulspace.clj/clj.base "0.8.3"]
                  [org.soulspace.clj/cmp.poi "0.6.2"]
---
+++ src/foss_report/io.clj
@@ -0,0 +1,47 @@
+(ns foss-report.io
+  (:require [clojure.edn :as edn]
+            [clojure.data.json :as json]
+            [clojure.data.csv :as csv]))
+
+
+(defn write-edn
+  "Writes the data in EDN format to the given writer."
+  [writer data]
+  (with-open [out writer]
+    (binding [*out* out]
+      (pr data))))
+
+(defn read-edn
+  "Reads the data in EDN format from the given reader."
+  [reader]
+  (with-open [in reader]
+    (edn/read-string (slurp in))))
+
+(defn write-csv
+  "Writes the data in CSV format to the given writer."
+  ([writer data]
+   (with-open [out writer]
+     (csv/write-csv out data)))
+  ([writer data options]
+   (with-open [out writer]
+     (csv/write-csv out data options))))
+
+(defn read-csv
+  "Reads the data in CSV format from the given reader."
+  [reader]
+  (with-open [in reader]
+    (doall
+     (csv/read-csv in))))
+
+(defn write-json
+  "Writes the data in JSON format to the given writer."
+  [writer data]
+  (with-open [out writer]
+    (spit out (json/write-str data))))
+
+(defn read-json
+  "Reads the data in JSON format from the given reader."
+  [reader]
+  (with-open [in reader]
+    (json/read-str (slurp in) :key-fn keyword)))
+
