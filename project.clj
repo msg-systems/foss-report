335889b690d0a10121ab1b5e7873270ef01bbaba
Ludger Solbach <ludger.solbach@msg.group>
2023-04-18 17:34:08 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -6,13 +6,13 @@
   ;:middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   ;:lein-tools-deps/config {:config-files [:install :user :project]}
 
-  ; use leiningen dependencies
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  ; use leiningen dependencies (workaround for windows)
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.4.0"]
                  [org.clojure/data.csv "1.0.1"]
                  [org.clojure/tools.cli "1.0.206"]
-                 [org.soulspace.clj/clj.base "0.8.3"]
-                 [org.soulspace.clj/cmp.poi "0.6.2"]
+                 [org.soulspace.clj/clj.base "0.9.1"]
+                 [org.soulspace.clj/cmp.poi "0.6.4"]
                  [org.soulspace.clj/tools.repository "0.3.4"]]
   
   :test-paths ["test"]
